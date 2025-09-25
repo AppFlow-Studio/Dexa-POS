1ae135ae83518de5b40e31ea3ad35cf3667de140
Temur662 <temurbeksayfutdinov@gmail.com>
2025-09-25 13:33:29 -0400

    Skeleton v.1.0.0

--- app/(main)/inventory/purchase-orders/index.tsx
+++ app/(main)/inventory/purchase-orders/index.tsx
@@ -231,12 +231,12 @@ const PurchaseOrdersScreen = () => {
       {activeTab === "purchase-orders" && (
         <View className="flex-1 bg-[#303030] border border-gray-700 rounded-xl">
           <View className="p-4 border-b border-gray-700">
-            <View className="gap-3">
-              <View>
+            <View className="gap-3 flex-row w-full  justify-between">
+              <View className="w-[70%]">
                 <Text className="text-gray-300 mb-1">Search (PO, Vendor, Employee, Status)</Text>
                 <TextInput value={query} onChangeText={setQuery} placeholder="e.g., PO-2025, Alice, Draft" placeholderTextColor="#9CA3AF" className="bg-[#212121] h-16 border border-gray-700 rounded-lg px-3 py-3 text-white" />
               </View>
-              <View>
+              <View className="w-[30%] " >
                 <Text className="text-gray-300 mb-1">Date Range</Text>
                 <DateRangePicker
                   startDate={startDate}
@@ -245,6 +245,7 @@ const PurchaseOrdersScreen = () => {
                     setStartDate(start);
                     setEndDate(end);
                   }}
+                  className="w-full h-16"
                   placeholder="Select date range for POs"
                 />
               </View>
@@ -301,14 +302,14 @@ const PurchaseOrdersScreen = () => {
         <View className="flex-1 bg-[#303030] border border-gray-700 rounded-xl">
           {externalExpenses.length > 0 ? (
             <View className="p-6">
-              <View className="bg-[#212121] border border-gray-600 rounded-lg p-4 mt-4">
+              {/* <View className="bg-[#212121] border border-gray-600 rounded-lg p-4 mt-4">
                 <View className="flex-row justify-between items-center">
                   <Text className="text-white font-bold text-lg">Total External Expenses</Text>
                   <Text className="text-green-400 font-bold text-xl">
                     ${externalExpenses.reduce((sum, expense) => sum + expense.totalAmount, 0).toFixed(2)}
                   </Text>
                 </View>
-              </View>
+              </View> */}
               <FlatList
                 data={externalExpenses}
                 keyExtractor={(item) => item.id}
