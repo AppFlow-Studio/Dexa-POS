9b3a71ab6f098e6d4dfc904d445f6d2b100b51e7
Temur662 <temurbeksayfutdinov@gmail.com>
2026-03-23 11:34:08 -0400

    Setting up CLEAN CFD

---
+++ contexts/CFDClientContext.tsx
@@ -0,0 +1,17 @@
+import { createContext, useContext } from "react";
+
+interface CFDClientActions {
+  sendTipSelection: (tipAmount: number, tipPercentage: number | null) => void;
+  reconnect: () => void;
+  disconnect: () => void;
+}
+
+const CFDClientContext = createContext<CFDClientActions | null>(null);
+
+export const CFDClientProvider = CFDClientContext.Provider;
+
+export function useCFDClient(): CFDClientActions {
+  const ctx = useContext(CFDClientContext);
+  if (!ctx) throw new Error("useCFDClient must be within CFDClientProvider");
+  return ctx;
+}
