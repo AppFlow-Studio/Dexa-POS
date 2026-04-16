dc24b669df37a6a48502064ce90fd7ca1da14300
Ali Jaffal <ajaffal126@gmail.com>
2026-04-16 13:33:53 +0300

    modifier items size fix

--- components/menu/ModifierScreen.tsx
+++ components/menu/ModifierScreen.tsx
@@ -134,13 +134,13 @@ const ModifierOption = memo(
       onPressIn={() => onToggle(categoryId, option.id)}
       onLongPress={() => onLongPress(categoryId, option.id)}
       delayLongPress={400}
-      className='rounded-lg border py-3 px-2.5 items-center justify-center'
+      className='rounded-lg border py-2 px-2 items-center justify-center'
       style={[
         {
-          flexBasis: '31%',
+          flexBasis: '18%',
           flexGrow: 0,
           flexShrink: 0,
-          minHeight: 72
+          minHeight: 52
         },
         isNo
           ? {
@@ -1584,6 +1584,7 @@ const ModifierScreenContent = () => {
                     flexWrap: 'wrap',
                     gap: 8,
                     alignItems: 'flex-start',
+                    justifyContent: 'center',
                     paddingBottom: 2
                   }}
                 >
