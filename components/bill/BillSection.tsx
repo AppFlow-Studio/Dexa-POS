2e9be05cdeb266b5494cbaeb96976fb9be124aea
Temur662 <temurbeksayfutdinov@gmail.com>
2026-04-20 08:01:00 -0400

    fix: removed extra # in order header

--- components/bill/BillSection.tsx
+++ components/bill/BillSection.tsx
@@ -751,7 +751,7 @@ const BillSectionContent = ({
               style={{ color: colors.heading, fontWeight: '700', fontSize: 18 }}
             >
               {activeOrderDisplayNumber
-                ? `Order #${activeOrderDisplayNumber}`
+                ? `Order ${activeOrderDisplayNumber}`
                 : 'New Order'}
             </Text>
             <TouchableOpacity
