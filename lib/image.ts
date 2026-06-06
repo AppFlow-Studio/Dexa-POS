b3bba2e687c7940b3441d3f5632e10622b154af4
Temur662 <temurbeksayfutdinov@gmail.com>
2026-06-06 11:54:23 -0400

    feat: updated logo

--- lib/image.ts
+++ lib/image.ts
@@ -3,5 +3,5 @@ export const images = {
   logo: require("../assets/images/logo.png"),
   loginBurger: require("../assets/images/login-burger.png"),
   topBar: require("../assets/images/top-bar.png"),
-  dexalogo: require("../assets/images/dexalogo.png"),
+  dexalogo: require("../assets/images/dexalogowhite.png"),
 };
