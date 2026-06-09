eae5187de9e475adfab498984f7d9aaecfd1e63b
Ali <ajaffal126@gmail.com>
2026-06-09 09:52:53 +0300

    fix first mount clicking for more options bottom sheet

--- app/(main)/order-processing.tsx
+++ app/(main)/order-processing.tsx
@@ -601,17 +601,20 @@ const OrderProcessing = () => {
 
   useEffect(() => {
     if (renderStage >= 2 && moreOptionsOpenedOnce) {
-      moreOptionsSheetRef.current?.expand();
+      const id = setTimeout(() => moreOptionsSheetRef.current?.expand(), 80);
+      return () => clearTimeout(id);
     }
   }, [moreOptionsOpenedOnce, renderStage]);
   useEffect(() => {
     if (renderStage >= 2 && discountOpenedOnce) {
-      discountSheetRef.current?.expand();
+      const id = setTimeout(() => discountSheetRef.current?.expand(), 80);
+      return () => clearTimeout(id);
     }
   }, [discountOpenedOnce, renderStage]);
   useEffect(() => {
     if (renderStage >= 2 && serviceChargeOpenedOnce) {
-      serviceChargeSheetRef.current?.expand();
+      const id = setTimeout(() => serviceChargeSheetRef.current?.expand(), 80);
+      return () => clearTimeout(id);
     }
   }, [renderStage, serviceChargeOpenedOnce]);
 
